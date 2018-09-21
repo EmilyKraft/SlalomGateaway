bd35287764a9a4c1ed3853995114a5f53346ebbf
Emily Kraft <ek1074@gmail.com>
2018-09-21 16:51:54 -0500

    ejecting webpack and babel and adding bootstrap

--- src/CreateBus.js
+++ src/CreateBus.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-// import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-//         <Form>
-//         <FormGroup>
-//         <Label>Departure Time: </Label>
-//         <Input type="time" label="Departure Time" />
-//         </FormGroup>
-//         <br />
-//         <FormGroup>
-//         <Label>Ride Type: </Label>
-//         <select name="rideType">
-//   <option value="Party">Party</option>
-//   <option value="Low-Key">Low-Key</option>
-//   <option value="Mixed">Mixed</option>
-// </select>
-// </FormGroup>
-// <br />
-// <FormGroup>
-//         <Label>Bus Information: </Label>
-//         <textarea name="message" rows="10" cols="30" />
-//         </FormGroup>
-//         <br />
-//         <Input type="submit" value="Create" />
-//         <Button>Cancel</Button>
-//         </Form>
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
 class CreateBus extends Component {
   render() {
     return (
       <div className="CreateBus">
-Hello there
+              <Form>
+              <FormGroup>
+              <Label>Departure Time: </Label>
+              <Input type="time" label="Departure Time" />
+              </FormGroup>
+              <br />
+              <FormGroup>
+              <Label>Ride Type: </Label>
+              <select name="rideType">
+        <option value="Party">Party</option>
+        <option value="Low-Key">Low-Key</option>
+        <option value="Mixed">Mixed</option>
+      </select>
+      </FormGroup>
+      <br />
+      <FormGroup>
+              <Label>Bus Information: </Label>
+              <textarea name="message" rows="10" cols="30" />
+              </FormGroup>
+              <br />
+              <Input type="submit" value="Create" />
+              <Button>Cancel</Button>
+              </Form>
       </div>
     );
   }
